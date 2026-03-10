88d1366377f8e45b2ad59fe0e401f482dbd11be0
Aleksandr-div <aleksandrkornienko978@gmail.com>
2026-03-11 00:31:46 +0300

    Update Unstable

--- main.rs
+++ main.rs
@@ -1,42 +1,64 @@
 use std::io::{self, Write};
-use std::fs::{self, OpenOptions};
+use std::fs::OpenOptions;
 
 fn main() {
     let mut todo: String = String::new();
     let mut answer: String = String::new();
+    let mut vec: Vec<String> = Vec::new();
 
     loop {
         todo.clear();
         answer.clear();
 
-        println!("Enter your todo: ");
-        io::stdin().read_line(&mut todo).expect("Error");
+        println!("Choose option: ");
+        println!("1. Add todo\n2. Delete todo\n3. Show my todos\n");
 
-        println!("Confirm? [y/n]");
         io::stdin().read_line(&mut answer).expect("Error");
 
-        if answer.trim() == "showtodos" || todo.trim() == "showtodos" {
-            let mut text = fs::read_to_string("db.txt").expect("Error");
-            println!("\nTodos: {}", text.lines().count());
-            println!("{}", &mut text);
-        }
-        
-        if answer.trim() == "y" {
-            let mut file: fs::File = OpenOptions::new()
+        if answer.trim() == "3" {
+            if vec.is_empty() {
+                println!("Thats nothing to do!\n");
+                continue;
+            }
+
+            for (i, value) in vec.iter().enumerate() {
+                println!("{}: {}\n", i + 1, value);
+            }
+
+        } else if answer.trim() == "2" {
+            if vec.is_empty() {
+                println!("Thats nothing to do!\n");
+                continue;
+            }
+
+            for (i, value) in vec.iter().enumerate() {
+                println!("\n{}. {}", i + 1, value);
+            }
+
+            answer.clear();
+            println!("Enter todo to delete: \n");
+            io::stdin().read_line(&mut answer).expect("Error");
+
+            if answer.trim().parse::<usize>().unwrap_or(0) > 0 {
+                vec.remove(answer.trim().parse::<usize>().expect("Error") - 1);
+                
+            } else {
+                vec.remove(0);
+            }
+
+        } else if answer.trim() == "1" {
+            println!("Enter todo: \n");
+            io::stdin().read_line(&mut todo).expect("Error");
+            vec.push(todo.clone().trim().to_string());
+
+            let mut file = OpenOptions::new()
+                .write(true)
                 .create(true)
-                .append(true)
+                .read(true)
                 .open("db.txt")
                 .expect("Error");
-
-            writeln!(file, "{}", todo.trim()).expect("Error");
-            println!("Succssesful");
-        } else if answer.trim() == "n" {
-            println!("Enter your todo: ");
-            io::stdin().read_line(&mut todo).expect("Error");
-        } else {
-            println!("Please enter y or n: ");
-            io::stdin().read_line(&mut answer).expect("Error");
+            writeln!(file, "{}", todo.clone()).expect("Error");
+            println!("Sucssesful!\n")
         }
     }
 }
-
